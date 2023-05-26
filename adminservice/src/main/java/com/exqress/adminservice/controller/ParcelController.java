4313a31e4a4377bd2a7a2e96473348047d50a2af
Kim jung won <k38836919@gmail.com>
2023-05-27 02:43:55 +0900

    프로젝트 초기 설정

---
+++ adminservice/src/main/java/com/exqress/adminservice/controller/ParcelController.java
@@ -0,0 +1,15 @@
+package com.exqress.adminservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class ParcelController {
+
+
+}
